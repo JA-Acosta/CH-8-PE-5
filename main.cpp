21f675bb6a876348bc89a26894358f269c7ba55a
JAAR <joe.aaron.acosta@outlook.com>
2023-06-05 19:07:46 -0400

    Version 1: Problem Prompt

---
+++ main.cpp
@@ -0,0 +1,38 @@
+// JAAR
+// 06/05/2023
+// C++ Primer Plus 6th Ed
+// CH 8 Program Ex 5
+// Version 1
+
+// Updates:
+
+// Future goals:
+// TODO: min5() add at inception
+// TODO: average value
+// TODO: most 
+
+// Potential Use cases/errors:
+
+// ------------------------------ Prompt---------------------------------------
+// template function max5() (argument is array of 5 T)
+// fixed size with max returning the largest element in the array.
+// ------------------------------ Prompt---------------------------------------
+
+// Headers:
+#include <iostream>
+
+using namespace std;
+
+// Global Constants and Structures:
+const int size = 5;
+
+// Prototypes:
+
+int main()
+{
+
+
+}
+
+
+// template
